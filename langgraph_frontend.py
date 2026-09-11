02c2c3265e38471a228018f615826120454009ce
Taushar lekhraj Maheshwari <141675858+Taushar@users.noreply.github.com>
2026-09-11 20:47:11 +0100

    Update langgraph_frontend.py

--- langgraph_frontend.py
+++ langgraph_frontend.py
@@ -38,11 +38,17 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = chatbot.invoke({'messages': [HumanMessage(content = user_input)]}, config = CONFIG)
-    ai_message = response['messages'][-1].content    
-    st.session_state['message_history'].append({'role': 'assitant' , 'content': user_input})
+    
     with st.chat_message('assistant'):
-            st.text(ai_message)
+        ai_message  = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content = 'how to brainstorm for an essay')]},
+                config =  {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode='messages'
+            )
+        )
+    st.session_state['message_history'].append({'role':'assistant', 'content': ai_message})
+            
 
 
 
