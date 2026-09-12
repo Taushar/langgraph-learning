f2955a1ef782d20b34f9f597ed42b7f2edd3db74
Taushar lekhraj Maheshwari <141675858+Taushar@users.noreply.github.com>
2026-09-12 16:04:46 +0100

    Update langgraph_frontend.py

--- langgraph_frontend.py
+++ langgraph_frontend.py
@@ -18,20 +18,21 @@ def generate_thread_id ():
     return thread_id
 
 def reset_chat():
-    thread_id = uuid.uuid4()
+    thread_id = str(uuid.uuid4())
     st.session_state['thread_id'] = thread_id
     add_thread(st.session_state['thread_id'])
     st.session_state['message_history'] = []
-    return thread_id
+    add_thread(thread_id)
 
 def add_thread(thread_id):
     if thread_id not in st.session_state['chat_threads']:
         st.session_state['chat_threads'].append(thread_id)
 
 def load_conversation(thread_id):
-    return chatbot.get_state(
+    state =  chatbot.get_state(
         config={'configurable': {'thread_id': thread_id}}
-    ).values['messages']      
+    )
+    return state.values.get('messages',[])      
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
@@ -67,32 +68,33 @@ with st.sidebar:
     if st.button("＋ New Chat", use_container_width=True):
         reset_chat()
 
-
-
     st.divider()
 
     st.caption("RECENT")
+
     for thread_id in st.session_state['chat_threads'][::-1]:
-        if st.sidebar.button(str(thread_id)):
-           st.session_state['thread_id'] = thread_id
-           messages = load_conversation(thread_id)
 
+        if st.sidebar.button(thread_id):
 
-           temp_messages = []
+            st.session_state['thread_id'] = thread_id
 
-           for message in messages :
-               if isinstance(message, HumanMessage):
-                   role = 'user'
+            messages = load_conversation(thread_id)
 
-               else:
-                   role = 'assistant' 
+            temp_messages = []
 
-               temp_messages.append({'role': role, 'content' : message.content})     
+            for message in messages:
 
+                if isinstance(message, HumanMessage):
+                    role = 'user'
+                else:
+                    role = 'assistant'
 
-           st.session_state['message_history'] = temp_messages
+                temp_messages.append({
+                    'role': role,
+                    'content': message.content
+                })
 
-
+            st.session_state['message_history'] = temp_messages
 
     
 
